50235a60b524406bc752358ce5cb2f281d81f9a9
fongelias <fongelias@gmail.com>
2018-05-21 10:58:01 -0400

    added dependency of comments to articles

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
 	# ApplicationRecord inherits from ActiveRecord::Base
 	# ActiveRecord::Base supplies a large amount of functionality, including CRUD
 	# associates articles with their comments:
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	# When validation fails on save, @article.save will return false
 	validates :title, presence: true, length: { minimum: 5 }
 	
